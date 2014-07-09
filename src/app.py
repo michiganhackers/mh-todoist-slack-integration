826065d61627a5c4e19e81e3e721bffb1cf84b9b
Jack Wink <jackwink@umich.edu>
2014-07-09 19:32:58 -0400

    link up task listing???

--- src/app.py
+++ src/app.py
@@ -7,13 +7,33 @@ import tornado.httpserver
 import conf
 from parser import MessageParser
 
+
 class MainHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.parser = MessageParser()
+
     def post(self):
-        if self.get_argument("user_name", conf.SLACK_USER) != conf.SLACK_USER:
-            m = MessageParser()
-            result = m.parse(self.get_argument('text', 'Hello, World!'))
-            if result["action"] == "list_group_todos":
-                self.write(json.dumps({"text": "Do me! Do me!"}))
+        # Don't reply to our own bot
+        if self.get_argument("user_name", conf.SLACK_USER) == conf.SLACK_USER:
+            return
+
+        msg = self.get_argument('text', '')
+        room = self.get_argument('channel_name', '')
+
+        result = self.parser.parse(room, msg)
+        if not result:
+            return
+
+        if result["action"] == "list_group_todos":
+            tasks = result["project"].get_uncompleted_tasks()
+            self.write(self.__format_tasks_for_reply(result["project"], tasks))
+
+    def __format_tasks_for_reply(self, project, tasks):
+        reply = ""
+        for task in tasks:
+            reply += task.content + " _due " + task.due_date + "_\n"
+        reply += "View at: https://todoist.com/app?v=301#project/" + project.id
+        return json.dumps({"text": reply, "mrkdwn": True})
 
 def main():
     application = tornado.web.Application([
--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,21 @@
 import re
+import todoist
 
 class MessageParser(object):
+    def __init__(self):
+        self.todo_api = todoist.Todoist()
+
     def _find_hashtags(self, msg):
         return re.findall(r"#(\w+)", msg)
 
-    def parse(self, msg):
+    def parse(self, room, msg):
+        project = self.todo_api.find_project(room)
+        if not project:
+            return None
+
         tags = self._find_hashtags(msg)
         for tag in tags:
             if tag == "todo":
-                return {"action": "list_group_todos" }
+                return {"action": "list_group_todos", "project": project }
 
-        return {"action": "none"}
+        return None
